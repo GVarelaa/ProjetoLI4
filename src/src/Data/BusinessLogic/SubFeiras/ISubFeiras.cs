5357f7c2594faa98abf03fe751686151b44bf6b9
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-01-08 23:32:59 +0000

    interface direita

--- src/src/Data/BusinessLogic/SubFeiras/ISubFeiras.cs
+++ src/src/Data/BusinessLogic/SubFeiras/ISubFeiras.cs
@@ -3,5 +3,18 @@ namespace src.Data.BusinessLogic.SubFeiras;
 
 public interface ISubFeiras
 {
+    public Task<IEnumerable<Feira>> GetFeiras();
+
+    public Task<Feira> GetFeira(string nome);
+
+    public Task<Produto> GetProduto(int id);
+
+    public Task<IEnumerable<Produto>> GetProdutos(string nomeFeira);
+
+    public void AddProduto();
+
+    public void AddFeira();
+
+
 }
 
