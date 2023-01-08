12bbf3028affc8e97b417906114f1402146f70c7
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-01-08 21:34:30 +0000

    Url

--- src/src/Data/Data/DAOConfig.cs
+++ src/src/Data/Data/DAOConfig.cs
@@ -3,9 +3,11 @@ namespace src.Data.Data;
 
 public class DAOConfig
 {
-    public const string Address = "localhost,3306";
-    public const string NomeBD = "UMarket";
-    public const string User = "root";
-    public const string Password = "root";
+    public const string ADDRESS = "localhost";
+    public const string DBNAME = "UMarket";
+    public const string USER = "teste";
+    public const string PASSWORD = "123";
+
+    public const string URL = "Data Souce=" + ADDRESS + ";Initial Catalog=" + DBNAME + ";User Id=" + USER + ";Password=" + PASSWORD + ";Trusted_Connection=true";
 }
 
